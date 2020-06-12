f446d46cbe9ee449e376d83634331e743c83187b
sakhimpungose <62238542+sakhimpungose@users.noreply.github.com>
2020-06-12 11:05:22 +0200

    Update Dice.py

--- Sprint 2/Dice.py
+++ Sprint 2/Dice.py
@@ -1,1 +1,14 @@
 
+class Dice():
+    
+    def __init__(self):
+        self.roll() # Initialising the dice with an appropriate random value
+        
+    def roll(self):
+        # Generate a random integer between 1 and 6 (both included)
+        self.__value = random.randint(1, 6)
+    
+    # String representation of the dice
+    def __str__(self):
+        return str(self.__value)
+    
